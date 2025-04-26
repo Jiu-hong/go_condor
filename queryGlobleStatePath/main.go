7780659a45650ea8159d86117fe2ec149d92f657
JH <jiu.hong.su@gmail.com>
2025-04-26 12:05:02 +0000

    add queryGlobalStatePath

---
+++ queryGlobleStatePath/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"go_condor/utils"
+	"net/http"
+
+	"github.com/make-software/casper-go-sdk/v2/casper"
+)
+
+func main() {
+
+	rpcClient := casper.NewRPCClient(casper.NewRPCHandler(utils.ENDPOINT, http.DefaultClient))
+	contractHash := "hash-082afb61e92f2b88013355502e17a2e3fa62a13eae2d134dc741c618b7eeb4d3"
+	result, err := rpcClient.QueryLatestGlobalState(context.Background(), contractHash, []string{"state_valset_nonce"})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	b, err := json.MarshalIndent(result.StoredValue, "", "  ")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Print(string(b))
+}
